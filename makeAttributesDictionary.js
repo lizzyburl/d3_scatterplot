eae1a1a899fdc5300b18ab0656ea0b3b85a58c66
xeroy <lizzyburl@gmail.com>
2012-07-24 11:26:34 -0400

    first commit

---
+++ makeAttributesDictionary.js
@@ -0,0 +1,108 @@
+//function to make a dictionary of attributes
+function makeAttributesDictionary(){
+	var attrData = window.attrData;
+	//get keys (i.e. gender, age, tissue type)
+	var keys = d3.keys(attrData[0].sample);
+	//create the html select element
+	d3.select("#selectColorCode")
+		.selectAll("option")
+		.data(keys)
+		.enter()
+		.append("option")
+		.attr("value", function(d){return d;})
+		.text(function(d){return d;});
+	//create a map for attributes
+	attributes = d3.map();
+	keys.forEach(function(key){
+	var typeIndicator = attrData[0].sample[key];
+		var attrValue = {
+			key : key,
+			dataType: typeof typeIndicator,
+		}
+		if (attrValue.dataType == "number"){
+			attrValue.colorInterpolator = createInterpolatorNumber(key);
+		}else if (attrValue.dataType == "string"){
+			listOfColorOptions(key);
+			attrValue.colorInterpolator = createInterpolatorString(key);
+		}
+		attributes.set(key, attrValue);
+		});
+		window.attributes = attributes;
+		
+} //end function make attributes array
+function listOfColorOptions(key){
+	var colorOptions = [];
+	//make array of different types for color interpolation domain
+	window.attrData.forEach(function(val){
+		if (colorOptions.indexOf(val.sample[key])<0)
+				colorOptions.push(val.sample[key]);
+});
+	window.colorTypes.set(key, colorOptions);
+}//end function listOfColorOptions
+
+function createInterpolatorString(key){
+	var colorInterpolator;
+	colorOptions = window.colorTypes.get(key);
+	if(colorOptions.length<=10){
+		var keyChecker = key.toLowerCase();
+
+	if((keyChecker== "gender")||(keyChecker=="sex")){
+		var colorRange = [];
+		colorOptions.forEach(function(gender, i){
+			genderLower = gender.toLowerCase();
+			if((genderLower == "male")||(genderLower == "man")||(genderLower == "boy"))
+				colorRange[i]="blue";
+			else if((genderLower == "female")||(genderLower == "woman")||(genderLower == "girl"))
+				colorRange[i]="fuchsia";
+			else
+				colorRange[i]="green";
+		});
+		colorInterpolator = d3.scale.ordinal()
+		.domain(colorOptions)
+		.range(colorRange);
+	}else{
+		colorInterpolator = d3.scale.category10()
+		.domain(colorOptions);
+		}
+	}
+	else if (colorOptions.length<=20){
+		colorInterpolator = d3.scale.category20()
+		.domain(colorOptions);
+	}
+	else{
+		var colorRange = [];
+		colorOptions.forEach(function(col, i){
+			var hueVal = i*280/colorOptions.length;
+			var hueColor =  "hsl(" + (hueVal) + ", 100%, 50%)";
+			colorRange[i] = hueColor;
+		});
+		//creates an ordinal scale interpolator based on the interpolated colors
+		colorInterpolator = d3.scale.ordinal()
+		.domain(colorOptions)
+		.range(colorRange);
+	}
+	return colorInterpolator;
+}//end of function createInterpolatorString
+function createInterpolatorNumber(key){
+		var color1 = "yellow";
+		var color2 = "blue";
+		var colorDomain = d3.extent(attrData, function(d){return d.sample[key]; });
+		//var keyMax = d3.max(attrData, function(d){return d.sample[key]; })
+		if (Math.abs(d3.hsl(color1).h - d3.hsl(color2).h)>200) //this is here in case we decide to go red -> purple or red -> blue
+			{
+				 colorInterpolator = d3.scale.linear()
+				.domain( colorDomain ) 
+				.interpolate(d3.interpolateRgb)
+				.range([color1,  color2]);
+			}
+		else 
+			{
+			color1 = d3.hsl(color1);
+			color2 = d3.hsl(color2);
+			colorInterpolator = d3.scale.linear()
+				.domain(colorDomain)
+				.interpolate(d3.interpolateHsl)
+				.range([color1, color2]);
+			}
+		return colorInterpolator;
+} //end of function createInterpolatorNumber
---
+++ makePlot.js
@@ -0,0 +1,144 @@
+function makePlot(geneX, geneY, studyID, n){
+	attrData = window.attrData;
+    attributes = window.attributes;
+	//gets height and width of div container
+	var width = parseInt(d3.select("#plot" + n).style("width"));
+	var height = parseInt(d3.select("#plot" + n).style("height"));
+	var padding = 4*Math.log(width) + 30;
+	
+	var gene_x = geneX;
+	var gene_y = geneY;
+    var studyid = studyID;
+	
+	var svg = d3.select("#plot" + n)
+	.append("svg")
+	.attr("id", "svg" + n)
+	.attr("width", width)
+	.attr("height", height);
+	
+	
+	//getting data from the database
+	//var data = {studyid: studyid, gene_x: gene_x, gene_y: gene_y};
+    //$.getJSON("http://yates.webfactional.com/studies/getData", data, function(d){
+	//getting data from my computer
+    $.getJSON("xyData.txt",  function(d){
+	
+		//finds the domain and range
+		var xMax = d3.max(d, function(d){return d.x; });
+	    var xMin = d3.min(d, function(d){return d.x; });
+	    var yMax = d3.max(d, function(d){ return d.y; });
+	    var yMin = d3.min(d, function(d){ return d.y; });
+	        		
+	    //sets the x- scale
+	   var xScale = d3.scale.linear()
+		.domain([xMin-(xMin*.1), xMax + (xMax*.05)])
+		.range([padding, width- padding]);
+	
+	    //sets the y- scale
+	    var yScale = d3.scale.linear()
+		.domain([yMin-(yMin*.1),yMax + (yMax*.05)])
+		.range([height - padding, padding]);
+		
+			makeTitles();
+function makeTitles(){	
+	//creates a title
+	svg.append("text")
+	.attr("id", "title")
+	.attr("text-anchor", "middle")
+	.attr("x", width/2- padding/2)
+	.attr("y", padding/2)
+	.text(studyid)
+	.attr("font-family", "sans-serif")
+	.attr("font-size", 10+width/70)
+	.attr("fill", "black");
+
+	//creates an x label
+	svg.append("text")
+	.attr("id", "x_label")
+	.attr("text-anchor", "middle")
+	.attr("x", width*.5- padding*.5)
+	.attr("y", height-padding*.2)
+	.text(gene_x)
+	.attr("font-family", "sans-serif")
+	.attr("font-size", padding*.22)
+	.attr("fill", "black");
+	
+	//creates a y label
+	svg.append("text")
+	.attr("id", "y_label")
+	.attr("text-anchor", "middle")
+	.attr("y", padding*.2 + 5)
+	.attr("x", -height/2)
+	.attr("transform", "rotate(-90)")
+	.text(gene_y)
+	.attr("font-family", "sans-serif")
+	.attr("font-size", padding*.22)
+	.attr("fill", "black");
+	
+	}//end function makeTitles
+	
+		//figures out selected colorCode
+		var colorCode = d3.select("#selectColorCode").property("value");
+			
+		svg.selectAll("circle")
+		.data(d)
+		.enter()
+		.append("circle")
+			.attr("cx", function(d){
+				return xScale(d.x);
+				})
+			.attr("cy", function(d) {
+				return yScale(d.y);
+				})
+			.attr("r", width*height*.00001)
+			.attr("class", "allCircles")
+			.attr("fill", function(d, i){
+			return attributes.get(colorCode).colorInterpolator(attrData[i].sample[colorCode]);
+				})
+			.style("opacity", .5)
+			.attr("stroke", "black")
+			.attr("stroke-width", .5)
+			.on("click", function(d, i){
+				fillInfoList(d, i);
+				makeRadiusBigger(this);
+				d3.event.stopPropagation();
+			})
+			.on("mouseover", function(d, i){rollOver(d, i, this, xScale, yScale)})
+			.on("mouseout", function(){rollOff();});
+			
+		var xAxis = d3.svg.axis()
+		.scale(xScale)
+		.orient("bottom")
+		.ticks(width*.015);  //Set rough # of ticks
+		
+		var yAxis = d3.svg.axis()
+		.scale(yScale)
+		.orient("left")
+		.ticks(height*.015);
+
+		svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(2," + (height - padding) + ")")
+		.call(xAxis);
+
+		svg.append("g")
+			.attr("class", "axis")
+			.attr("transform", "translate(" +( padding + 2 )+ ",0)")
+			.call(yAxis);
+
+		d3.select("#plot" + n).append("div")	
+			.attr("class", "pointText")
+			.attr("id", "pointText")
+			.style("font-size", 13)
+			.style("font-family", "sans-serif")
+			.style("display", "none")
+			.style("background", "white")
+			.style("height", 17)
+			.style("min-width", 10)
+			.style("max-width", 200)
+			.style("position", "absolute");	
+			
+//	selecting(svg, xScale, yScale);
+	
+	}); //ends getJSON
+} //ends function makePlot
